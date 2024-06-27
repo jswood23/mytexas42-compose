3d07974b9807d7ecda2c6acb4284c9481ccc2dd3
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-06-27 12:10:10 -0500

    update docker files to use .env

--- reset-docker.bat
+++ reset-docker.bat
@@ -4,6 +4,5 @@ docker system prune -a
 pause
 docker volume rm mytexas42-compose_postgres-data
 pause
-set /p POSTGRESPW=Set PostgreSQL password:
-docker-compose up -d -e POSTGRESPW=%POSTGRESPW%
+docker-compose up -d
 pause
