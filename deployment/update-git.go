06eb3b9597fdc10bd0223efa9cd062020fe367b5
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-07-05 16:32:37 -0500

    update git pull command 3

--- deployment/update-git.go
+++ deployment/update-git.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"mytexas42-compose/system"
 	"os"
+	"strings"
 )
 
 func updateGit(environment string) error {
 	println(fmt.Sprintf("Updating %s from latest version.", environment))
 
-	var backendPath string
-	var frontendPath string
+	var backendPath, frontendPath string
 	if environment == "staging" {
 		backendPath = os.Getenv("BACKEND_STAGING_REPO")
 		frontendPath = os.Getenv("FRONTEND_STAGING_REPO")
@@ -19,15 +19,27 @@ func updateGit(environment string) error {
 		frontendPath = os.Getenv("FRONTEND_PRODUCTION_REPO")
 	}
 
-	err := system.Run("git", "-C", backendPath, "pull")
-
-	if err != nil {
+	// Function to handle git pull and possible dubious ownership error
+	handleGitPull := func(path string) error {
+		err := system.Run("git", "-C", path, "pull")
+		if err != nil && strings.Contains(err.Error(), "dubious ownership") {
+			// Attempt to add the directory to safe.directory configuration
+			safeDirErr := system.Run("git", "config", "--global", "--add", "safe.directory", path)
+			if safeDirErr != nil {
+				return safeDirErr // Return error if unable to add to safe.directory
+			}
+			err = system.Run("git", "-C", path, "pull") // Retry git pull
+		}
 		return err
 	}
 
-	err = system.Run("git", "-C", frontendPath, "pull")
+	// Update backend repository
+	if err := handleGitPull(backendPath); err != nil {
+		return err
+	}
 
-	if err != nil {
+	// Update frontend repository
+	if err := handleGitPull(frontendPath); err != nil {
 		return err
 	}
 
